af69db150e3671ed9c87beba17181445e1139fbb
kiran18072009-ai <kiran18072009@gmail.com>
2026-04-06 20:49:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,5 +102,8 @@ def calculator():
     return render_template("calculator.html",)
 
 
-if __name__ == '__main__':
-    app.run()
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
